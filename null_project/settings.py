25be3fbc96c746b343893dbbcbc7929a0d16d3b9
Omisola Idowu Paul <57432464+omisolaidowu@users.noreply.github.com>
2020-05-19 12:42:33 +0100

    Update settings.py

--- null_project/settings.py
+++ null_project/settings.py
@@ -129,9 +129,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'djangohost', 'static')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'djangohost', 'media')
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
